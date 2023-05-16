8574f0eabc95c79a59f284055e68a73a1cba177f
Shibasish Das <shibasish.das@1und1.de>
2023-05-17 00:49:13 +0200

    mqtt topic listener

--- src/main/kotlin/com/circulariot/backend/config/MqttConfig.java
+++ src/main/kotlin/com/circulariot/backend/config/MqttConfig.java
@@ -1,22 +1,13 @@
 package com.circulariot.backend.config;
 
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.core.MessageProducer;
-import org.springframework.integration.dsl.IntegrationFlow;
-import org.springframework.integration.dsl.IntegrationFlows;
-import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
-import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
 import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
 import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
-import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.MessageHandler;
-import org.springframework.messaging.MessagingException;
 
 @Configuration
 @EnableIntegration
@@ -26,18 +17,15 @@ public class MqttConfig {
     private static final String CLIENT_ID = "spring-boot-mqtt";
 
     @Bean
-    public MqttConnectOptions mqttConnectOptions() {
-        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
-        mqttConnectOptions.setAutomaticReconnect(true);
-        mqttConnectOptions.setCleanSession(false);
-        return mqttConnectOptions;
-    }
-
-    @Bean
-    public MqttPahoClientFactory mqttClientFactory() {
-        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
-        factory.setConnectionOptions(mqttConnectOptions());
-        return factory;
+    public MessageProducer inbound() {
+        MqttPahoMessageDrivenChannelAdapter adapter =
+                new MqttPahoMessageDrivenChannelAdapter(BROKER_URL, CLIENT_ID,
+                        "topic1", "topic2");
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        adapter.setOutputChannel(mqttInputChannel());
+        return adapter;
     }
 
     @Bean
@@ -45,21 +33,4 @@ public class MqttConfig {
         return new DirectChannel();
     }
 
-//    @Bean
-//    public MessageProducer inbound() {
-//        MqttPahoMessageDrivenChannelAdapter adapter =
-//                new MqttPahoMessageDrivenChannelAdapter(BROKER_URL, CLIENT_ID, mqttClientFactory(), "topic/test");
-//        adapter.setCompletionTimeout(5000);
-//        adapter.setConverter(new DefaultPahoMessageConverter());
-//        adapter.setQos(1);
-//        return adapter;
-//    }
-
-//    @Bean
-//    public IntegrationFlow mqttInFlow() {
-//        return IntegrationFlows.from(inbound())
-//                .channel(mqttInputChannel())
-//                .handle(handler())
-//                .get();
-//    }
 }
--- src/main/kotlin/com/circulariot/backend/listener/MqttMessageListener.java
+++ src/main/kotlin/com/circulariot/backend/listener/MqttMessageListener.java
@@ -1,12 +1,15 @@
 package com.circulariot.backend.listener;
 
 
+import jakarta.websocket.MessageHandler;
 import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
 
 @MessageEndpoint
-public class MqttMessageListener {
+@Component
+public class MqttMessageListener implements MessageHandler {
 
     @ServiceActivator(inputChannel = "mqttInputChannel")
     public void handleMessage(Message<String> message) {
